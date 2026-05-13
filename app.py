67cdf41084a6e216948aaecad218d356e74359a8
Luiz Pellanda <luiz_pellanda@hotmail.com>
2026-05-12 21:39:44 -0400

    Evolving the code - basic conditional

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-print("Sabor Express")
+print('''
+      Sabor Express
+      ''')
+
+# Para referencia - fsymbols
 
 print("1. Register Restaurant")
 print("2. List Restaurants")
 print("3. Activate Restaurant")
-print("4. Exit")
+print('4. Exit\n')
+
+chosen_option = input('Select an option: ')
+
+if chosen_option == '1':
+    print('You selected option 1 - Register Restaurant')
+elif chosen_option == '2':
+    print('You selected option 2 - List Restaurants')
+elif chosen_option == '3':
+    print('You selected option 3 - Activate Restaurant')
+elif chosen_option == '4':
+    print('Exiting the program. Goodbye!')
+else:
+    print('Invalid option selected. Please try again.')
