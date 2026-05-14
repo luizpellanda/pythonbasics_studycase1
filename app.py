0937d4d81cf9e2c753b1b6fd9bb2ea3c7e042359
Luiz Pellanda <luiz_pellanda@hotmail.com>
2026-05-14 17:56:02 -0400

    Added validation to the menu

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ def  show_program_name():
     print('''
           Sabor Express 
         ''')
+    
+def invalid_option():
+       print('Invalid option. Please select a valid option.')
+       input('Press Enter to go back to main menu...')
+       main()
+
 
 def show_menu():
     print("1. Register Restaurant")
@@ -18,24 +24,27 @@ def end_app():
     print('Exiting the program. Goodbye!')
 
 def handle_user_selection():
-    chosen_option = int(input('Select an option: '))
+    try:
+        chosen_option = int(input('Select an option: '))
 
-    match chosen_option:
-        case 1:
-            print('You selected option 1 - Register Restaurant')
-        case 2:
-            print('You selected option 2 - List Restaurants')
-        case 3:
-            print('You selected option 3 - Activate Restaurant')
-        case 4:
-            end_app()
-        case _:
-            print('Invalid option. Please select a valid option.')
+        match chosen_option:
+            case 1:
+                print('You selected option 1 - Register Restaurant')
+            case 2:
+                print('You selected option 2 - List Restaurants')
+            case 3:
+                print('You selected option 3 - Activate Restaurant')
+            case 4:
+                end_app()
+            case _:
+                invalid_option()
+    except ValueError:
+        invalid_option()
 
 def main():
     show_program_name()
     show_menu()
     handle_user_selection()
-    
+
 if __name__ == '__main__':
     main()
